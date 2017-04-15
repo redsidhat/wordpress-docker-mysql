e0144312ac9e7fbf83f7c5d44fa93d8177eede99
Appu A S <appu.as@livestream.com>
2017-04-15 23:00:27 +0530

    removing the hardcoded pub key

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,6 +1,4 @@
 resource "aws_key_pair" "wp-key" {
   key_name   = "wp-key"
-  public_key = "ssh-rsa
-AAAAB3NzaC1yc2EAAAADAQABAAABAQCkUe5U6pQ6iZe76DTJMfLNllY4hhsCg6fiyFOAGizwqt+xyMEF2qH/2SrWWAUtWMzt4sP53E6DGZeEsXIf7vDhxajPWHDQyhijAqb7ua5Mc4Mq5z0y1HlNiZ/nZFkZpBjsjtld0g+hFoC04nZ4vQ41CvXlxA2mgx8Nfa9WMkEHypGJ1Go1dvbLqq0++QyxdR22iQcCLJj/gs0TIiNFtHXpOQUeu3j5gUAzZ1IjKxfRMpKMerBblrA0PsDQcgJ4Vb0HjwUaC541xDSN78xLjvig37Raap4Dpa+Uik5zdSQGtFPFZAyV7PSq85sed+I7hgVCO1mgw5vYXFVq+VMP0N/p
-appu@Appus-Macbook-Pro.local"
+  public_key = ""
 }
