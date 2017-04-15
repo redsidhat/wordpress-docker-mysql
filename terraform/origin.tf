a88c8c2b66e30be520b44196d4ea73b4196d0d97
Appu A S <appu.as@livestream.com>
2017-04-16 00:00:47 +0530

    security group and ec2

---
+++ terraform/origin.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "origin" {
+    ami = "${data.aws_ami.ubuntu.id}"
+    #region = "${var.azone}"
+    instance_type = "t2.small"
+    associate_public_ip_address = "true"
+#    security_groups = ["allow_ssh", "allow_internal", "allow_http"]
+    key_name = "${aws_key_pair.wp-key.key_name}"
+    tags {
+        Name = "origin"
+    }
+}
---
+++ terraform/security_groups.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh ports"
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "allow_http" {
+  name = "allow_http"
+  description = "Allow all http"
+
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
